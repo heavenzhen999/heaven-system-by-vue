ffe4b2983686d142cfe92599ee8d86223f883af7
heavenzhen999 <heavenzhen999@163.com>
2018-10-22 22:50:57 +0800

    modified:   src/null.vue

--- src/null.vue
+++ src/null.vue
@@ -1,3 +1,11 @@
+/*
+ * @Author: chen zhen
+ * @Date: 2018-10-22 22:50:13
+ * @Last Modified by: chen zhen
+ * @Last Modified time: 2018-10-22 22:50:13
+ * @Description 我是版本3
+ */
+
 <template>
   <div></div>
 </template>
