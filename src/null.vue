0d328aa18ecb33ef6147e118d3374233f577d745
heavenzhen999 <heavenzhen999@163.com>
2018-10-22 22:43:47 +0800

    modified:   src/null.vue

--- src/null.vue
+++ src/null.vue
@@ -1,11 +1,3 @@
-/*
- * @Author: chen zhen
- * @Date: 2018-10-22 22:20:09
- * @Last Modified by: chen zhen
- * @Last Modified time: 2018-10-22 22:20:09
- * @Description 修改了 change1
- */
-
 <template>
   <div></div>
 </template>
